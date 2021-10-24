13d35efed18a36dde43eac254d5d6d8bb024e199
DavidUmansky <71831586+DavidUmansky@users.noreply.github.com>
2021-10-24 14:51:56 +0300

    Changing all the project
    adding end game and fixing alot of things in the previuos code

--- Speed Trivia/script.js
+++ Speed Trivia/script.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('button');
 const introContent = document.querySelector('.hero-text');
 const gameContent = document.querySelector('#gameContainer');
+const endContent = document.querySelector('#endGame');
 const imgBG = document.querySelector('.hero-image');
 const question = document.getElementsByClassName('arrow-pointer')[0];
 const logo = document.querySelector('#myLogo2');
@@ -10,21 +11,35 @@ const answers = document.getElementsByClassName('ansSpan');
 const divQ = document.querySelector('.down');
 const secSpan = document.querySelector('.timer')
 const answers2 = document.getElementsByClassName('answers');
+const answersLetter = document.getElementsByClassName('span2');
 const body = document.getElementsByTagName('body');
 const phoneImg = document.getElementsByClassName('test');
- 
+const messageImg = document.getElementsByClassName('happy');
+const messageContent = document.getElementsByClassName('messegeC');
+const messegeSign = document.getElementById('likeTop'); 
+
+
+let messege = document.getElementsByClassName('messege'); 
+let subject;
+let subject2;
+let l; // 0-3 to get right answer
+let count = 0;
+let gameO_counter = 0;
+let x; 
+let correct = 0;
+
+
 if (window.innerWidth < 421) {
     imgBG.classList.add('test');
 }
 
-let count = 0;
-
 const enterGame = () => {
-    let subject = selects[0].value;
+    subject = selects[0].value;
+    subject2 = selects[0].value.charAt(0).toLowerCase() + selects[0].value.slice(1);
     if (subject === 'Music') {
         subject = 12
     } else {
-        if(subject === 'Entertainment: Films') {
+        if(subject === 'Films') {
             subject = 11
         } else {
             if(subject === 'Sports') {
@@ -43,7 +58,7 @@ const enterGame = () => {
     
     const level = selects[1].value.charAt(0).toLowerCase() + selects[1].value.slice(1);
         
-    if (subject === 'Choose questions subject' || level === 'Choose difficulty level') {
+    if (subject === 'Choose questions subject' || level === 'choose difficulty level') {
         return;
     }
 
@@ -55,7 +70,20 @@ const enterGame = () => {
     introContent.classList.add('hero-text2');
     imgBG.classList.add('hero-image2');
     logo.style.display = 'none';
-    
+    const gameContainer = document.querySelector('.hero-text2');
+     
+    function endM() {
+        x = 0;
+        introContent.innerHTML = '';
+        introContent.appendChild(endContent)
+        endContent.style.display = 'block'
+        introContent.style.backgroundColor = 'initial';
+        introContent.style.opacity = 'initial';
+        introContent.classList.add('hero-text3')
+        endContent.style.backgroundColor = 'initial';
+        endContent.style.opacity = 'initial';
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+    } 
 
     const httpRequest = new XMLHttpRequest();
     httpRequest.responseType = 'json';
@@ -64,12 +92,87 @@ const enterGame = () => {
         if (httpRequest.status === 200) {
           let responsi = httpRequest.response.results
           function insertQ() {
+            count++
+    
+            if (count > 10 && gameO_counter < 3) {
+                endM();
+                messege[0].innerHTML = `You won the 1,000,000$ with ${correct} right answers! your knowladge in the ${subject2} subject is excellent. try higher level 
+                /play with another subject.`
+                
+                if (window.innerWidth < 421) {
+                    for(let y = 0; y < 2; y++) {
+                        messageImg[y].parentNode.removeChild(messageImg[y])
+                        }                
+                        messageImg[0].style.height = '148.1px' 
+                        messageImg[0].style.width = '100%'
+                        messageContent[0].style.height = '275px'
+                        messege[0].style.marginTop = '-0.6px'
+                        document.getElementById('fixArrow').src = 'return.png';
+                        document.getElementById('firstI').style.marginTop = '-9px';
+                    }
+                
+                if (window.innerWidth < 415 && window.innerWidth > 410) {
+                    messageImg[0].style.height = '168px'
+                    document.getElementById('fixArrow').style.marginTop = '10px';
+                    document.getElementById('video').style.marginTop = '5px'
+                }                
+
+                if (window.innerWidth < 322 && window.innerWidth > 318) {
+                    messageImg[0].style.height = '128.2px'
+                    messegeSign.style.width = '62.5%'; 
+                }
+
+                if (window.innerWidth < 290 && window.innerWidth > 270) {
+                    messageImg[0].style.height = '91px'
+                    messageImg[0].style.width = '100.7%' 
+                    document.getElementById('firstI').style.marginTop = '-6px';
+                }                 
+                
+                return;
+            }
+
+            if (gameO_counter === 3) {
+                console.log('Game Over, stop game and enter message');
+                endM();
+                messegeSign.innerHTML = '&#128078'
+                messege[0].innerHTML = `You have 3 wrong answers, game over &#128542 try again.`;
+                for(let x = 0; x < 3; x++) {
+                    messageImg[x].src = 'failed.gif';
+                    messageImg[x].style.height = '167px'
+                }
+                messege[0].style.backgroundColor = '#de5e55'
+                messageContent[0].style.border = '5px inset #4b1d24'
+                messageContent[0].style.borderTop = '6px inset rgb(199 38 62)'
+            
+                if (window.innerWidth < 421) {
+                    for(let y = 0; y < 2; y++) {
+                        messageImg[y].parentNode.removeChild(messageImg[y])
+                        }                
+                        messageImg[0].style.height = '148px' 
+                        document.getElementById('fixArrow').src = 'return.png';
+                        messegeSign.style.width = '61%';
+                        document.getElementById('fixArrow').style.marginTop = '9px';
+
+                    }
+                
+                if (window.innerWidth < 290) {
+                    messageImg[0].style.height = '127.5px'
+                    messege[0].style.marginTop = '-0.7px'
+                }
+                
+                return;
+            }
+
+            l = Math.floor((Math.random() * 4));
             let i = responsi.length;
             let j = Math.floor((Math.random() * i));
             let h = 0;
             spanQ.innerHTML = responsi[j].question ;
-            answers[Math.floor((Math.random() * 4))].innerHTML = responsi[j].correct_answer    
-            
+            answers[l].innerHTML = responsi[j].correct_answer    
+            answers2[l].classList.add('correct');
+            answers[l].classList.add('correct');
+            answersLetter[l].classList.add('correct');
+
             while (h < 3) {
                 let i = Math.floor((Math.random() * 4));
                 if(answers[i].innerHTML === '') {
@@ -90,23 +193,53 @@ const enterGame = () => {
                 }      
             }
 
-            count++
+            if (window.innerWidth < 290 && window.innerWidth > 270) {
+                const imgBG2 = document.querySelector('.hero-image2');
+
+                let i = 0;
+                while (i < 4) {
+                    if (divQ.innerText.length > 85) {
+                        imgBG2.style.overflow = 'hidden';
+                        divQ.style.width = '235px'
+                    } else {
+                        imgBG2.style.overflow = 'initial';
+                        divQ.style.width = '185px'
+                    }
+                    i++
+                }
+            }
+
+            if (window.innerWidth < 340 && window.innerWidth > 320) {
+                const imgBG2 = document.querySelector('.hero-image2');
+
+                let i = 0;
+                while (i < 4) {
+                    if (divQ.innerText.length > 116) {
+                        divQ.style.width = '260px'
+                    } else {
+                        divQ.style.width = '240px'
+                    }
+                    i++
+                }      
+            }
+
+
+     
             responsi.splice(j, 1);
         
           }
             
           insertQ()
 
-        //   secSpan.innerHTML = 30;
           let sec_value = 31;
 
           let intervalId = setInterval(secondDown,1000)  
         
           function secondDown() {
- 
-            // if (secSpan.innerHTML === '01') {
-            //     clearInterval(intervalId);
-            // }                         
+            
+            if (x === 0) {
+                clearInterval(intervalId);
+            }                         
 
             let i = '0';
             sec_value--;
@@ -114,34 +247,56 @@ const enterGame = () => {
             if (sec_value < 10) {
                 sec_value = i + sec_value 
             }
- 
-            // if (sec_value === 30) {
-            //     secSpan.style.visibility = 'initial'    
-            // }
+              
+            // padding-left: 11.5px;
+            
+            secSpan.innerHTML = sec_value;
+            
+            if (sec_value < 20 && sec_value > 9) {
+                secSpan.style.paddingLeft = '11.5px'
+                secSpan.style.justifyContent = 'initial'
+            } else {
+                secSpan.style.justifyContent = 'center'
+                secSpan.style.paddingLeft = 'initial'
+            }
 
-            secSpan.innerHTML = sec_value;              
+            //     justify-content: center;
+
 
             if (+sec_value < 10) {
                 secSpan.style.backgroundColor = 'red'
             }
  
             if (secSpan.innerHTML === '00') {
+                gameO_counter++
                 more()
             }
 
            }
            
-           function more() {
+           function more(e) {
+                
+                if(e) {
+                    if (e.target.className.includes("correct") === false) {
+                        gameO_counter++
+                    } else {
+                        correct++
+                    }
+                }
+                 
                 for (let i = 0; i < 4; i++) {
                   answers[i].innerHTML = ''; 
                 }
                 
                 secSpan.innerHTML = 30;
                 sec_value = 31;
-                // secSpan.style.visibility = 'hidden';
                 secSpan.style.backgroundColor = 'rgb(9, 170, 9)'
-
+                
+                answers2[l].classList.remove('correct');
+                answers[l].classList.remove('correct');
+                answersLetter[l].classList.remove('correct');
                 insertQ()
+                        
             }
         
            for (let i = 0; i < 4; i++) {
@@ -153,13 +308,9 @@ const enterGame = () => {
     httpRequest.open('GET', 'https://opentdb.com/api.php?amount=10&category=' + subject + '&difficulty=' + level + '&type=multiple');
     httpRequest.send();
 
-    
-    
 }
 
 button.addEventListener('click',enterGame)
-
-
 
 
 
